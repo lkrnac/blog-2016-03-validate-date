fcf7a20fa8a13f19f35dd2c7fe631f7ec1890910
Lubos Krnac <lubos.krnac@gmail.com>
2016-03-20 18:01:42 +0000

    Add date verification examples

---
+++ src/test/java/net/lkrnac/blog/validatedate/DateValidationTest.java
@@ -0,0 +1,58 @@
+package net.lkrnac.blog.validatedate;
+
+import org.exparity.hamcrest.date.DateMatchers;
+import org.exparity.hamcrest.date.LocalDateTimeMatchers;
+import org.exparity.hamcrest.date.ZonedDateTimeMatchers;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by: Lubos Krnac
+ * For: Dotsub LLC.
+ * Date: 2016-03-19.
+ */
+public class DateValidationTest {
+    @Test
+    public void validateDateTime() {
+        //GIVEN
+        LocalDateTime expectedDateTime = LocalDateTime.now();
+
+        //WHEN
+        LocalDateTime actualDateTime = LocalDateTime.now();
+
+        //THEN
+        assertThat(actualDateTime, LocalDateTimeMatchers.within(2, ChronoUnit.SECONDS, expectedDateTime));
+    }
+
+    @Test
+    public void validateInstant() {
+        //GIVEN
+        ZonedDateTime expectedDateTime = ZonedDateTime.of(2016, 3, 20, 13, 3, 0, 0, ZoneId.of("GMT+1"));
+
+        //WHEN
+        ZonedDateTime actualDateTime = ZonedDateTime.of(2016, 3, 20, 13, 3, 0, 0, ZoneId.of("GMT-0"));
+
+        //THEN
+        assertThat(actualDateTime, ZonedDateTimeMatchers.sameDay(expectedDateTime));
+        assertThat(actualDateTime, ZonedDateTimeMatchers.after(expectedDateTime));
+    }
+
+    @Test
+    public void validateDate() {
+        //GIVEN
+        Date expectedDate = new Date();
+
+        //WHEN
+        Date actualDate = new Date();
+
+        //THEN
+        assertThat(actualDate, DateMatchers.within(2, ChronoUnit.SECONDS, expectedDate));
+    }
+}
